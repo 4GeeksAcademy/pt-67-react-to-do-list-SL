f8ba874d870d149a1b1211cf84d3ff5cbb3000aa
Sonia <161872642+Inflanubes@users.noreply.github.com>
2024-05-27 19:40:56 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tareas, setTareas] = useState(["Wash my hands", "Make homework"])
+	const [nuevaTarea, setNuevaTarea] = useState("")
+	
+	
+	const handleTareaNueva = (event) => {
+		setNuevaTarea(event.target.value)
+	}
+
+	const handleSubmit = (event) => {
+		event.preventDefault()
+		setTareas([...tareas,nuevaTarea])
+	}
+
+	const handleDelete = (index) => {
+		const newArr = []
+		for (let i = 0; i < tareas.length; i++) {
+			if (i !== index) {
+				newArr.push(tareas[i])
+			}
+		}
+		setTareas(newArr)
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	
+		<div className="w-50 m-auto mt-5">
+			<form onSubmit={handleSubmit}>
+				<div className="mb-3">
+					<label htmlhtmlFor="exampleInputEmail1" className="form-label">Add your task here</label>
+					<input onChange={handleTareaNueva} type="text" className="form-control" id="newTask" aria-describedby="taskHelp"/>
+					<div id="taskHelp" className="form-text">You'll never forguet a task again.</div>
+				</div>
+				<button type="submit" className="btn btn-primary">Submit</button>
+			</form>
+			
+			
+			<ul className="w-50 m-auto">
+				{/* Utilizamos el método map para generar dinámicamente los elementos <li> */}
+				{tareas.map((item, index) => (
+				<li key={index}>{item}
+					<button onClick={()=>handleDelete(index)} type="button" className="btn btn-primary">X</button>
+				</li>
+				))}
+			</ul>
 		</div>
+		
+
 	);
 };
 
